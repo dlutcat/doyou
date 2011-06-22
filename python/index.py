4e67431a14effaf1d7274eb7c62ef8dc1350f9b9
dlutat <pat.inside@gmail.com>
2011-06-22 20:32:28 +0800

    test

---
+++ python/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env pthon
+# -*- coding: utf-8 -*-
+
+import web
+from web import form
+
+import settings
+
+render = web.template.render(settings.TEMPLATE_DIR)
+
+urls = (
+    '/', 'index',
+)
+
+app = web.application(urls, globals())
+
+class index:
+
+    def GET(self):
+        return render.index()
+
+if __name__ == '__main__': app.run()
