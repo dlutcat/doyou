6c5f992ff37375f0cfd83f8d518d13cf2172b679
dlutcat <pat.inside@gmail.com>
2011-06-23 20:24:56 +0800

    -

---
+++ index.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import web
+from web import form
+from web.contrib.template import render_jinja
+
+import settings
+
+#render = web.template.render(settings.TEMPLATE_DIR)
+render = render_jinja(
+    'templates',
+    encoding='utf-8',
+)
+
+urls = (
+    '/', 'index',
+    '/home/', 'home',
+    '/test/', 'test',
+)
+
+app = web.application(urls, globals(), autoreload=True)
+
+class index:
+
+    def GET(self):
+        return render.index()
+
+class home:
+
+    def GET(self):
+        return render.home()
+
+class test:
+    
+    def GET(self):
+        return render.test(name='hello')
+
+if __name__ == '__main__': app.run()
