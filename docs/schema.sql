1472ce6036c16c7bc579bb78ea5ca504e2fdb285
dlutcat <pat.inside@gmail.com>
2011-07-01 20:29:58 +0800

    DB schema

---
+++ docs/schema.sql
@@ -0,0 +1,17 @@
+-- User
+-- id, nickname, email, gender, location, password, avatar, profile, shorturl, create_time
+
+-- oauth
+-- id, oauth_type, oauth_id, oauth_homepage, create_time
+
+-- question
+-- id, content, asker, category, *answer_num*, create_time
+
+-- answer
+-- id, qid, flag, comment, create_user, create_time
+
+-- message
+-- id, title, content, type, create_time
+
+-- message_users
+-- message_id, from_uid, to_uid
