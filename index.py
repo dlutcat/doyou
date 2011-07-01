8531725a4f5bd8f0d9207e812216983d1a30727a
dlutcat <pat.inside@gmail.com>
2011-07-01 20:30:25 +0800

    Framework changed to Flask

--- index.py
+++ index.py
@@ -1,54 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import web
-from web import form
-from web.contrib.template import render_jinja
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-import settings
-from models import *
 
-render = render_jinja(
-    'templates',
-    encoding='utf-8',
-)
-'''
-render = web.template.render('templates')
-'''
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-urls = (
-    '/', 'index',
-    '/home/', 'home',
-    '/test/', 'test',
-)
-
-user_form = form.Form(
-    form.Textbox('name', description='Username'),
-    form.Textbox('fullname', description='Fullname'),
-    form.Password('password', description='password'),
-    form.Password('password2', description='Repeat passowrd'),
-    form.Button('password2', description='Repeat passowrd')
-)
-
-app = web.application(urls, globals(), autoreload=True)
-
-class index:
-
-    def GET(self):
-        return render.index()
-
-class home:
-
-    def GET(self):
-        return render.home()
-
-class test:
-    
-    def POST(self):
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    error = None
+    if 'POST' == request.method:
         pass
-            
-    def GET(self):
-        f = user_form()
-        return render.test(f)
+    else:
+        return render_template('register.html')
 
-if __name__ == '__main__': app.run()
+@app.route('/home/')
+def home():
+    return render_template('home.html')
+
+@app.route('/test/')
+def test():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
