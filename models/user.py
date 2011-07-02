3e5276bbf2b49f9ab38413bcf8cf80363a95a975
dlutcat <pat.inside@gmail.com>
2011-07-02 18:11:21 +0800

    -

--- models/user.py
+++ models/user.py
@@ -1,36 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-#from settings
-
-#engine = create_engine('mysql://%s:%s@%s/%s' % (settings.USER, settings.PASSWORD, settings.HOST, settings.DBNAME)
-engine = create_engine('mysql://root:deadbeef@localhost/doyou')
-Base = declarative_base()
-
-'''
-metadata = MetaData()
-users_table = Table('users', metadata,
-  Column('id', Integer, primary_key=True),
-  Column('name', String(30)),
-  Column('fullname', String(50)),
-  Column('password', String(30))
-)
-
-metadata.create_all(engine)
-
-class User(object):
-  def __init__(self, name, fullname, password):
-    self.name = name
-    self.fullname = fullname
-    self.password = password
-  def __repr__(self):
-    return "<User('%s', '%s', '%s')>" % (self.name, self.fullname, self.password)
-
-mapper(User, users_table)
-'''
+from sqlalchemy import Column, Integer, String
+from doyou.database import Base
 
 class User(Base):
   __tablename__ = 'users'
@@ -47,9 +19,3 @@ class User(Base):
 
   def __repr__(self):
     return "<User('%s', '%s', '%s')>" % (self.name, self.fullname, self.password)
-
-users_table = User.__table__
-metadata = Base.metadata
-
-if __name__ == '__main__':
-    metadata.create_all(engine)
